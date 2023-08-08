802ef839f5d3b9fd18531fdcc3f9826350be25fa
lakshaycode22 <lakshaycode22@gmail.com>
2023-08-09 03:02:10 +0530

    fixed projects for mobile

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,7 +18,7 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt className="green-pink-gradient p-[1px] rounded-2xl shadow-card mt-20 h-full">
+      <Tilt className="green-pink-gradient p-[1px] rounded-2xl shadow-card mt-3">
         <div
           options={{
             max: 45,
@@ -85,7 +85,7 @@ const Works = () => {
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="my-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
